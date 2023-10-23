43caf1648f265d45d17ab9183f2bfc687c45dadc
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-23 14:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your code here
+// Get references to all the image divs
+const divs = document.querySelectorAll('.image');
+
+let draggedItem = null;
+
+// Add dragstart event listeners to all divs
+divs.forEach(div => {
+  div.addEventListener('dragstart', (e) => {
+    // Store the dragged item
+    draggedItem = div;
+  });
+
+  div.addEventListener('dragend', () => {
+    // Clear the dragged item
+    draggedItem = null;
+  });
+
+  div.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  div.addEventListener('dragenter', (e) => {
+    e.preventDefault();
+  });
+
+  div.addEventListener('drop', () => {
+    if (draggedItem !== null && draggedItem !== div) {
+      // Swap background images
+      const temp = div.style.backgroundImage;
+      div.style.backgroundImage = draggedItem.style.backgroundImage;
+      draggedItem.style.backgroundImage = temp;
+    }
+  });
+});
